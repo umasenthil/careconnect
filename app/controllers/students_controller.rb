6f979e3db5384f4fbaf2c2e0376882446f45fbae
Uma Narayana <uma_senthil@hotmail.com>
2018-10-25 03:25:05 +0000

    Controller to create Student record

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,14 @@ class StudentsController < ApplicationController
   	@student = Student.new
   end
 
+  def create
+  	Student.create(student_params)
+  	redirect_to root_path
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:FirstName, :LastName, :skypeID)
+  end
+
 end
