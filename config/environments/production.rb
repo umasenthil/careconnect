c73fb831bbfea264c36e33eed97247f7d6dcb18e
Uma Narayana <uma_senthil@hotmail.com>
2018-10-26 05:05:36 +0000

    Added devise support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  #Added for devise
+  config.action_mailer.default_url_options = { host: 'careconnect-umasenthil.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
